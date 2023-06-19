e48e98f7ef92f00190f53a73ecfa5b5bca61f249
TaymourBreiche <133786296+TaymourBreiche@users.noreply.github.com>
2023-06-19 15:48:15 -0400

    Update Practice.java

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -16,6 +16,7 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Practice");
         System.out.println("HEHE");
+        System.out.println("Nope");
         // TODO code application logic here
     }
     
